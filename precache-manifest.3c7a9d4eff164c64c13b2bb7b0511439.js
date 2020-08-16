9b39a8cfd6ddc4d75cf16dc633ba4974de66cb55
Jesse McKinney <jessemckinney513@gmail.com>
2020-08-16 16:15:17 -0400

    Updates

---
+++ precache-manifest.3c7a9d4eff164c64c13b2bb7b0511439.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5bbd0396ce6f1fa2c3d23379fd6b328e",
+    "url": "/Slainte/index.html"
+  },
+  {
+    "revision": "c379f89df68c6ae057d3",
+    "url": "/Slainte/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "a55e76ea0af241cb1d63",
+    "url": "/Slainte/static/js/2.87e52947.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Slainte/static/js/2.87e52947.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c379f89df68c6ae057d3",
+    "url": "/Slainte/static/js/main.17bef649.chunk.js"
+  },
+  {
+    "revision": "a579e25c779f3966d2a8",
+    "url": "/Slainte/static/js/runtime-main.90658dfa.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Slainte/static/media/logo.5d5d9eef.svg"
+  }
+]);
