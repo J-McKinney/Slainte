fe564a81f34c1680c2b3fd147acaee80a63e6c1d
Jesse McKinney <jessemckinney513@gmail.com>
2020-08-17 14:30:23 -0400

    yay

--- src/Components/BeerContainer/BeerContainer.jsx
+++ src/Components/BeerContainer/BeerContainer.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Gears2 from "../../Images/gears2.png";
+import curvedDiv from "../../curvedDiv.svg";
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Style from "./BeerContainer.module.css";
@@ -75,14 +76,35 @@ function BeerContainer(props) {
             </Row>
             <Row>
               <div className={Style.bottomFunnelToCatchMilledGrain}>
-                <div className={Style.bottomFunnelToCatchMilledGrainOutline} />
+                <div className={Style.bottomFunnelToCatchMilledGrainOutline}>
+                  <img src={curvedDiv} alt="curved div" />
+                </div>
               </div>
             </Row>
             <Row>
-              <div className={Style.innerPipetoMashingArea}>
-                <div className={Style.outerPipeToMashingArea} />
+              <div className={Style.outerPipeToMashingArea1}>
+                <div className={Style.innerPipetoMashingArea1} />
               </div>
             </Row>
+            <Row>
+              <div className={Style.outerPipeToMashingArea2}>
+                <div className={Style.innerPipetoMashingArea2} />
+              </div>
+              <div className={Style.outerCornerPipeToMashingArea2}>
+                <div className={Style.innerCornerPipetoMashingArea2} />
+              </div>
+            </Row>
+            <svg
+              className={Style.grainPile}
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 1440 320"
+            >
+              <path
+                fill="#555"
+                fillOpacity="1"
+                d="M0,160L48,138.7C96,117,192,75,288,58.7C384,43,480,53,576,85.3C672,117,768,171,864,165.3C960,160,1056,96,1152,101.3C1248,107,1344,181,1392,218.7L1440,256L1440,320L1392,320C1344,320,1248,320,1152,320C1056,320,960,320,864,320C768,320,672,320,576,320C480,320,384,320,288,320C192,320,96,320,48,320L0,320Z"
+              />
+            </svg>
           </Container>
         </div>
       </div>
