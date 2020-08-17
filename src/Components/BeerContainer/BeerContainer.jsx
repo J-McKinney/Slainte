a30e596be16d95e5c6ea0fcbf579ec19015afd8a
Jesse McKinney <jessemckinney513@gmail.com>
2020-08-17 13:41:31 -0400

    yay

--- src/Components/BeerContainer/BeerContainer.jsx
+++ src/Components/BeerContainer/BeerContainer.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Gears2 from "../../Images/gears2.png";
+import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Style from "./BeerContainer.module.css";
 
@@ -8,37 +9,81 @@ function BeerContainer(props) {
     <>
       <div className={Style.wrapper}>
         <div className={Style.secondWrapper}>
-          {/* top of funnel */}
-          <div className={Style.trapezoid} />
-          {/* Milling the grain */}
-          <Row className={Style.firstRectangleRow}>
-            {/* gears to grind up the grain */}
-            <div className={Style.rectangle1}>
-              {/* gear box */}
-              <Row className={Style.firstGearRow}>
-                {/* first set of gears */}
-                <img className={Style.firstGearTop} src={Gears2} alt="Gears2" />
-                <img className={Style.secondGearTop} src={Gears2} alt="Gears2" />
-                <img className={Style.thirdGearTop} src={Gears2} alt="Gears2" />
-              </Row>
-              <Row className={Style.secondGearRow}>
-                {/* second set of gears */}
-                <img className={Style.firstGearMiddle} src={Gears2} alt="Gears2" />
-                <img className={Style.secondGearMiddle} src={Gears2} alt="Gears2" />
-              </Row>
-              <Row className={Style.thirdGearRow}>
-                {/* third set of gears */}
-                <img className={Style.firstGearBottom} src={Gears2} alt="Gears2" />
-                <img className={Style.secondGearBottom} src={Gears2} alt="Gears2" />
-                <img className={Style.thirdGearBottom} src={Gears2} alt="Gears2" />
-              </Row>
+          <Container>
+            <div className={Style.funnelWrapper}>
+              {/* top of funnel */}
+              <div className={Style.topFunnelToCatchGrain}>
+                <div className={Style.topFunnelToCatchGrainOutline} />
+              </div>
+              <div className={Style.ovalShapeOnTopOfTopFunnel} />
             </div>
-            {/* The grain has been milled and now about to move to mashing */}
-            {/* empty rectangle to have pipe-like-flow look */}
-            <div className={Style.emptyRectangle1} />
-          </Row>
-          {/* pipe to mashing area */}
-          <div className={Style.rectangle2}></div>
+            {/* Milling the grain */}
+            <Row className={Style.firstRectangleRow}>
+              {/* gears to grind up the grain */}
+              <div className={Style.gearBox1}>
+                {/* gear box */}
+                <Row className={Style.firstGearRow}>
+                  {/* first set of gears */}
+                  <img
+                    className={Style.firstGearTop}
+                    src={Gears2}
+                    alt="Gears2"
+                  />
+                  <img
+                    className={Style.secondGearTop}
+                    src={Gears2}
+                    alt="Gears2"
+                  />
+                  <img
+                    className={Style.thirdGearTop}
+                    src={Gears2}
+                    alt="Gears2"
+                  />
+                </Row>
+                <Row className={Style.secondGearRow}>
+                  {/* second set of gears */}
+                  <img
+                    className={Style.firstGearMiddle}
+                    src={Gears2}
+                    alt="Gears2"
+                  />
+                  <img
+                    className={Style.secondGearMiddle}
+                    src={Gears2}
+                    alt="Gears2"
+                  />
+                </Row>
+                <Row className={Style.thirdGearRow}>
+                  {/* third set of gears */}
+                  <img
+                    className={Style.firstGearBottom}
+                    src={Gears2}
+                    alt="Gears2"
+                  />
+                  <img
+                    className={Style.secondGearBottom}
+                    src={Gears2}
+                    alt="Gears2"
+                  />
+                  <img
+                    className={Style.thirdGearBottom}
+                    src={Gears2}
+                    alt="Gears2"
+                  />
+                </Row>
+              </div>
+            </Row>
+            <Row>
+              <div className={Style.bottomFunnelToCatchMilledGrain}>
+                <div className={Style.bottomFunnelToCatchMilledGrainOutline} />
+              </div>
+            </Row>
+            <Row>
+              <div className={Style.innerPipetoMashingArea}>
+                <div className={Style.outerPipeToMashingArea} />
+              </div>
+            </Row>
+          </Container>
         </div>
       </div>
     </>
