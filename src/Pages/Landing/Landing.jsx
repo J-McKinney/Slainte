6154339f2cf498e590cf67942e4592f3edbd6dc1
Jesse <jessemckinney513@gmail.com>
2020-09-30 09:46:31 -0400

    yay

--- src/Pages/Landing/Landing.jsx
+++ src/Pages/Landing/Landing.jsx
@@ -1,11 +1,14 @@
 import React, { Component } from "react";
+import BaseGrain from "../../baseGrainContainer.svg";
 import Style from "./Landing.module.css";
 
 class Landing extends Component {
   render() {
     return (
       <>
-        <div className={Style.wrapper}></div>
+        <div className={Style.wrapper}>
+          <img src={BaseGrain} alt="base grain" />
+        </div>
       </>
     );
   }
